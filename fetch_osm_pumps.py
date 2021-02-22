efdd6c314b198c131f3ee6a2237c83076ab15493
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-02-22 17:26:54 +0100

    chore: Add todo

--- fetch_osm_pumps.py
+++ fetch_osm_pumps.py
@@ -33,6 +33,7 @@ def fetch_osm_pumps(path, outpath):
     query_string = "http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%3B%28area%5B%22ISO3166%2D2%22%3D%22DE%2DBE%22%5D%5B%22admin%5Flevel%22%3D%224%22%5D%3B%29%2D%3E%2EsearchArea%3B%28node%5B%22man%5Fmade%22%3D%22water%5Fwell%22%5D%5B%22description%22%3D%22Berliner%20Straßenbrunnen%22%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%3E%3Bout;"
 
     # get data
+    # TODO: [GDK-17] Wrap requests into a new function and test that it gives us back a JSON
     r = requests.get(query_string)
 
     gdf = get_overpass_gdf(r.json())
